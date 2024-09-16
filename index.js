9e9290aa3c078da50c4bf580dea54f72cd936a25
Nilton Soares <niltoncarlos433@gmail.com>
2024-09-16 19:10:54 -0300

    Erro de código 2

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { select, input } = require('@inquirer/prompts');
+const { select, input, checkbox } = require('@inquirer/prompts');
 
 let meta = {
     value: 'Tomar 3L de água por dia',
@@ -18,7 +18,31 @@ const cadastrarMetas = async () => {
     metas.push(
         { value: meta, checked: false }
     );
-};
+}
+
+const listarMetas = async () => {
+    const respostas = await checkbox({
+        message: "Use as setas para mudar de meta, o espaço para marcar ou desmarcar e o Enter para finalizar essa etapa",
+        choices: [...metas], 
+        instructions: false,
+    })
+
+    if(respostas.length == o){
+        console.log("Nenhuma meta selecionada!")
+        return
+    }
+
+    respostas.forEach((resposta) => {
+        const meta = metas.find((m) => {
+            return m.value == resposta
+        })
+
+        meta.checked = true
+    })
+
+    console.log('Meta(s) marcada(s) como concluída(s)')
+
+}
 
 const start = async () => {
     while (true) {
@@ -26,8 +50,8 @@ const start = async () => {
             message: "Menu >",
             choices: [
                 {
-                    name: "Cadastrarmetas",
-                    value: "cadastrar" // Corrigido aqui
+                    name: "Cadastrar metas",
+                    value: "cadastrar" 
                 },
                 {
                     name: "Listar metas",
@@ -46,8 +70,8 @@ const start = async () => {
                 console.log(metas);
                 break;
             case "listar":
-                console.log("Vamos listar as metas:");
-                metas.forEach(m => console.log(m.value)); // Lista as metas
+                await listarMetas();
+                console.log(metas);
                 break;
             case "sair":
                 console.log("Até a próxima!");
